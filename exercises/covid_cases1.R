ad6fb9c464bc1af67ca0ca63308d025a5eebe7e7
Jonathan Marshall <jmarshall@never.you.mind>
2021-02-02 13:44:20 +1300

    adds covid exercise for transformation2

---
+++ exercises/covid_cases1.R
@@ -0,0 +1,20 @@
+# COVID-19 cases in NZ
+library(tidyverse)
+library(readxl)     # for reading from excel
+library(janitor)    # for some tidying up
+
+# Read in the case data in
+covid_excel = read_excel("data/covid_arrivals/covid-cases-30july20.xlsx")
+
+# We see it contains junk at the top. You could load the Excel sheet into Excel to see this too if you want!
+covid_excel
+
+# We can get rid of the junk by skipping the first 3 rows
+covid_excel = read_excel("data/covid_arrivals/covid-cases-30july20.xlsx", skip=3)
+
+# We tidy up the column names (e.g. get rid of spaces, make uniform capitalisation)
+covid = covid_excel %>% clean_names()
+
+# And do a plot
+ggplot(covid) +
+  geom_bar(mapping=aes(x=date_notified_of_potential_case))
---
+++ exercises/covid_cases2.R
@@ -0,0 +1,20 @@
+# COVID-19 cases in NZ
+library(tidyverse)
+library(readxl)     # for reading from excel
+library(janitor)    # for some tidying up
+library(lubridate)  # for dates
+
+# Read in the case data in and tidy up
+covid = read_excel("data/covid_arrivals/covid-cases-30july20.xlsx", skip=3) %>%
+  clean_names()
+
+# Take a look at the type of data we have. Notice that the dates are of type dttm (Date time)
+glimpse(covid)
+
+# Grab just the overseas cases, and find how long after arrival they were a case. Differences
+# in datetimes will be in seconds; the time_length() command converts to different units.
+overseas = covid %>%
+  filter(overseas_travel == "Yes") %>%
+  mutate(difference = date_notified_of_potential_case - arrival_date,
+         length = time_length(difference, unit="day"))
+overseas
