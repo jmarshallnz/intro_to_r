75e90ca57b5cc0fc7603f88b9f436f186c6f7dd3
Jonathan Marshall <jmarshall@never.you.mind>
2021-02-15 12:24:24 +1300

    code to compile the solution zip file

--- create_zip.R
+++ create_zip.R
@@ -4,3 +4,8 @@ exercise_files <- list.files("exercises", pattern = "*.R", full.names = TRUE)
 data_files <- list.files("data", recursive = TRUE, full.names = TRUE)
 all_files <- c("intro_to_r.Rproj", exercise_files, data_files)
 zip('intro_to_r.zip', all_files)
+
+solution_files <- list.files("solutions", pattern = "*.R", full.names = TRUE)
+data_files <- list.files("data", recursive = TRUE, full.names = TRUE)
+all_files <- c("intro_to_r.Rproj", solution_files, data_files)
+zip('intro_to_r_solutions.zip', all_files)
